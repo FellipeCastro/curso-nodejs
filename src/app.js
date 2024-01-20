14eb0024d7c3863afcc01752b18fdf3796a04767
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-20 10:31:41 -0300

    aula 05 finalizada

--- src/app.js
+++ src/app.js
@@ -7,9 +7,19 @@ app.use(express.json())
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'}
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'}    
 ]
+
+// Returnar o objeto por Id
+const buscarSelecaoPorId = (id) => {
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+// Pegar a posição do elemento no array por id
+const buscarIndexSelecao = (id) => {
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
 
 // Rota padrão/raiz
 app.get('/', (req, res) => {
@@ -20,9 +30,19 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com id: ${req.params.id} excluida com sucesso!`)
 })
 
 export default app
