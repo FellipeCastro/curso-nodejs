90c1d0dab60c65ad34e751098a52b62c2383cc1b
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-22 18:07:12 -0300

    aula 09 finalizada

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 import app from './src/app.js'
-import conexao from './infra/conexao.js'
 
 const PORT = 5500
 
-// Conexão
-conexao.connect((err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Conexão realizada com sucesso')
-
-        // Escutar porta 5500
-        app.listen(PORT, () => {
-            console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-        })
-    }
+// Escutar porta 5500
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 import express from 'express'
+import conexao from '../infra/conexao.js'
 const app = express()
+
 // Indicando para o Express ler o body com JSON
 app.use(express.json())
-
-// Mock (estrutura base de dados para teste)
-const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suíça', grupo: 'G'},
-    {id: 3, selecao: 'Camarões', grupo: 'G'},
-    {id: 4, selecao: 'Sérvia', grupo: 'G'}    
-]
 
 // Returnar o objeto por Id
 const buscarSelecaoPorId = (id) => {
@@ -20,36 +14,77 @@ const buscarSelecaoPorId = (id) => {
 const buscarIndexSelecao = (id) => {
     return selecoes.findIndex(selecao => selecao.id == id)
 }
-
-// Rota padrão/raiz
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+// Rotas
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes)
+    // res.status(200).send(selecoes)
+    const sql = 'SELECT * FROM selecoes;'
+    conexao.query(sql, (error, result) => {
+        if (error) {
+            res.status(404).json({"erro": error})
+        } else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 app.get('/selecoes/:id', (req, res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
+    // res.json(buscarSelecaoPorId(req.params.id))
+    const id = req.params.id
+    const sql = 'SELECT * FROM selecoes WHERE id=?;'
+    conexao.query(sql, id, (error, result) => {
+        const linha = result[0]
+        if (error) {
+            res.status(404).json({"erro": error})
+        } else {
+            res.status(200).json(linha)
+        }
+    })
 })
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
-    res.status(201).send('Seleção cadastrada com sucesso')
+    // selecoes.push(req.body)
+    // res.status(201).send('Seleção cadastrada com sucesso')
+    const selecao = req.body
+    const sql = 'INSERT INTO selecoes SET ?;'
+    conexao.query(sql, selecao, (error, result) => {
+        if (error) {
+            res.status(400).json({"erro": error})
+        } else {
+            res.status(201).json(result)
+        }
+    })
 })
 
 app.delete('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSelecao(req.params.id)
-    selecoes.splice(index, 1)
-    res.send(`Seleção com id: ${req.params.id} excluida com sucesso!`)
+    // let index = buscarIndexSelecao(req.params.id)
+    // selecoes.splice(index, 1)
+    // res.send(`Seleção com id: ${req.params.id} excluida com sucesso!`)
+    const id = req.params.id
+    const sql = 'DELETE FROM selecoes WHERE id=?;'
+    conexao.query(sql, id, (error, result) => {
+        if (error) {
+            res.status(404).json({"erro": error})
+        } else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 app.put('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSelecao(req.params.id)
-    selecoes[index].selecao = req.body.selecao
-    selecoes[index].grupo = req.body.grupo
-    res.json(selecoes)
+    // let index = buscarIndexSelecao(req.params.id)
+    // selecoes[index].selecao = req.body.selecao
+    // selecoes[index].grupo = req.body.grupo
+    // res.json(selecoes)
+    const id = req.params.id
+    const selecao = req.body
+    const sql = 'UPDATE selecoes SET ? WHERE id=?;'
+    conexao.query(sql, [selecao, id], (error, result) => {
+        if (error) {
+            res.status(400).json({"erro": error})
+        } else {
+            res.status(200).json(result)
+        }
+    })
 })
 
 export default app
