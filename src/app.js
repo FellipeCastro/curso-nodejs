44e1824383053a0504ea0c4a854a1951caebc37d
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-24 08:48:29 -0300

    aula 14 finalizada

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from 'express'
 import routes from './routes.js'
 const app = express()
 
-// Usar o router
-app.use(routes)
-
 // Indicando para o Express ler o body com JSON
 app.use(express.json())
+
+// Usar o router
+app.use(routes)
 
 export default app
